3ef868c0fc0b9c37d1e14f3df3a027d315f96dc2
Tanzim Ahmed <mr.tanzim.ahmed@gmail.com>
2025-07-04 01:06:45 +0600

    Update

--- src/test/java/com/parabank/parasoft/testCases/LoginTest.java
+++ src/test/java/com/parabank/parasoft/testCases/LoginTest.java
@@ -1,6 +1,7 @@
 package com.parabank.parasoft.testCases;
 
 import com.parabank.parasoft.pages.LoginPage;
+import com.parabank.parasoft.util.ParaBankUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -9,9 +10,16 @@ import org.testng.annotations.Test;
 public class LoginTest extends BaseTest {
     //private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
     @Test
-    public void checkLoginPage(){
+    public void checkLoginPage() {
         LoginPage loginPage = page.goTo(LoginPage.class);
         Assert.assertTrue(loginPage.isLoginPage());
+    }
+
+    @Test
+    public void checkLoginPageTitle() {
+        LoginPage loginPage = page.goTo(LoginPage.class);
+        String title = loginPage.getPageTitle();
+        Assert.assertEquals(title, ParaBankUtil.LOGIN_PAGE_TITLE, "Login Page Title Matched");
     }
 
     @Test
@@ -32,11 +40,12 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
-    public  void loginShouldFailWithPassword(){
+    public void loginShouldFailWithPassword() {
         LoginPage loginPage = page.goTo(LoginPage.class);
         loginPage = loginPage
                 .fillPassword("asdsada")
                 .clickLoginButtonAndStay();
         Assert.assertTrue(loginPage.hasLoginError(), "Login Should fail with only password");
     }
+
 }
